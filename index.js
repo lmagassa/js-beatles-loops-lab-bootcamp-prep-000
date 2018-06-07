9b31c4d05bbcb8f5ba464068f05b6cb963674cf6
Lassana Magassa <mystuff@lassanamagassa.com>
2018-06-07 09:31:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+//Beatles Loops returns an array of strings 
+//containing what instruments eachmusician plays
+
+function theBeatlesPlay(theMusician, theInstrument) {
+  var musiciansInstrument = []
+  for(var i = 0; i < theMusician.length; i++) {
+    musiciansInstrument = [...musiciansInstrument, `${theMusician[i]} plays ${theInstrument[i]}`]
+  }
+  return musiciansInstrument
+}
+
+function johnLennonFacts(theFacts) {
+  var counter = 0
+  var confirmedFacts = []
+    //var theFacts = `${theFacts[counter]}!!!`
+  while(counter < theFacts.length) {
+    //console.log(theFacts[counter] + '!!!')
+    var confirmedFacts = [...confirmedFacts, `${theFacts[counter]}!!!`]
+    counter++
+  }
+return confirmedFacts
+}
+
+function iLoveTheBeatles(aNumber) {
+  var allLove = []
+  do {
+    [...allLove, `I love the Beatles!`]
+  } while (aNumber < 15)
+  return allLove
+}
